2e32e9cbfc157e0c9d31f2e38f79269d49d78dce
pprost <pjprobst@stu.naperville203.org>
2022-08-23 15:07:50 -0500

    learned bluej and took notes

---
+++ myfirstprogram/main.java
@@ -0,0 +1,33 @@
+
+/**
+ * First repo program
+ * Preston Probst
+ * 8/23/22
+ * Who helped me: 
+ */
+public class main
+{
+    public static void main(String[] args)
+    {
+        System.out.print("Hello world\n");
+        //basic data types
+        double num = 9.6,number2;
+        
+        int i = 3;
+        boolean choice = true;
+        char c = 'P';
+        float f = 8;
+        long l1 = 5;
+        short s6 = 6;
+        
+        //these are classes and n1 and word are objects made by them
+        //Class    ID       memory  constructor
+        //Name     
+        Integer    n1 =     new     Integer(66);
+        String     word =   new     String("Kevin Hayes");
+        Double     n5 =     new     Double(6.7);
+        
+        System.out.print(word.length());
+        
+    }
+}
